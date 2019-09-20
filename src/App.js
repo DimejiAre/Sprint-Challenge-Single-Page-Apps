70eaa24c2695c54d25ef3fa58644e815c4bba786
DimejiAre <olardi2@gmail.com>
2019-09-20 08:32:39 +0100

    add routing to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
-
+import WelcomePage from "./components/WelcomePage";
+import CharacterList from "./components/CharacterList";
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path='/' render={props => <WelcomePage {...props} />} />
+      <Route path='/characters' render={props => <CharacterList {...props} />} />
     </main>
   );
 }
