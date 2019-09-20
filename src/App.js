2d53921ba976ba22ff304cc165a7160ae1ecc01e
DimejiAre <olardi2@gmail.com>
2019-09-20 09:38:20 +0100

    add SearchForm component and render

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
+import SearchForm from "./components/Form";
+import axios from "axios";
 
 export default function App() {
 
   const [characters, setCharacters] = useState([]);
 
+  const search = (formValue, actions) => {
+    const matchedCharacters = characters.filter( chr => {
+      if(chr.name.toLowerCase().includes(formValue.name)){
+        return chr
+      }
+    })
+    setCharacters(matchedCharacters);
+    actions.resetForm();
+  }
+
+  const validationSchema = {a:'boy'};
+
   return (
     <main>
       <Header />
       <Route exact path='/' render={props => <WelcomePage {...props} />} />
+      <Route path='/characters' 
+        render={props => <SearchForm 
+        {...props} 
+        search={search} 
+        validationSchema={validationSchema} />}
+      />
       <Route path='/characters' 
         render={props => <CharacterList 
         {...props} 
