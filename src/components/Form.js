37e671e0672d87d017d36a7a062045234b4cdced
DimejiAre <olardi2@gmail.com>
2019-09-20 09:37:48 +0100

    create SearchForm

---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+function SearchForm (props) {
+    const {search, validationSchema} = props;
+
+    return (
+        <Formik 
+            validationSchema={validationSchema}
+            initialValues={{name: ''}}
+            onSubmit={search}
+            render={props => {
+                return (
+                <Form>
+                    <label>Search by Name</label>
+                    <Field name='name' type='text' placeholder='Enter Name' />
+                    <ErrorMessage name='name' component='div' />
+                    <button type="submit">Enter</button>
+                </Form>
+                )
+            }}
+        />
+    );
+}
+
+export default SearchForm;
