1ae11aa97b78c184e906979988dfccc7efb9e85f
DimejiAre <olardi2@gmail.com>
2019-09-20 08:53:36 +0100

    add state for characters and render CharacterList component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
 
 export default function App() {
+
+  const [characters, setCharacters] = useState([]);
+
   return (
     <main>
       <Header />
       <Route exact path='/' render={props => <WelcomePage {...props} />} />
-      <Route path='/characters' render={props => <CharacterList {...props} />} />
+      <Route path='/characters' 
+        render={props => <CharacterList 
+        {...props} 
+        characters={characters} 
+        setCharacters={setCharacters} />}
+      />
     </main>
   );
 }
